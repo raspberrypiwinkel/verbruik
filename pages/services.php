e976e0758850c5da4fb79524407e2e21dc326dfe
Netorigin <info@netorigin.nl>
2019-12-07 22:53:06 +0100

    upd

--- pages/services.php
+++ pages/services.php
@@ -23,8 +23,6 @@ $active_page = 'services';
 		}
 	}
 
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -55,7 +53,7 @@ $active_page = 'services';
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="./index.php"><img src="./static/images/raspberry.png" />Verbruiks App</a>
+				<a class="navbar-brand" href="./index.php"><img src="../static/images/raspberry.png" />Verbruiks App</a>
 			</div>
 			<div id="navbar" class="navbar-collapse collapse">
 				<ul class="nav navbar-nav navbar-right">
@@ -80,7 +78,7 @@ $active_page = 'services';
 
 							foreach($active_services_arr AS $a)
 							{
-								echo '<tr><td>'.$a.'</td><td><form method="post" action="./actions.php" style="display:inline-block;"><input type="hidden" name="sname" value="'.$a.'"><input type="hidden" class="form-control" name="action" value="stop_sname"><button type="submit" class="btn btn-xs btn-danger" onclick="return confirm(\'Are you sure to stop '.$a.'?\')">Stop</button></form></td></tr>';
+								echo '<tr><td>'.$a.'</td><td><form method="post" action="../actions.php" style="display:inline-block;"><input type="hidden" name="sname" value="'.$a.'"><input type="hidden" class="form-control" name="action" value="stop_sname"><button type="submit" class="btn btn-xs btn-danger" onclick="return confirm(\'Are you sure to stop '.$a.'?\')">Stop</button></form></td></tr>';
 							}
 						?>
 						</table>
