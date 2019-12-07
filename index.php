b46c9c2fc4ad52cc5ab8563ee87195a0935ed491
Netorigin <info@netorigin.nl>
2019-12-07 21:45:43 +0100

    update

--- index.php
+++ index.php
@@ -183,7 +183,7 @@ $(function() {
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="./index.php"><img src="./static/images/raspberry.png" />Verbruiks App</a>
+				<a class="navbar-brand" href="./index.php"><img src="./static/images/raspberry.png" />Raspberrypiwinkel.nl Verbruiks App</a>
 			</div>
 			<div id="navbar" class="navbar-collapse collapse">
 				<ul class="nav navbar-nav navbar-right">
@@ -221,8 +221,7 @@ include_once('./include/menu.php');
 							<div class="col-xs-6 col-sm-3 text-center">
 								<span class="chart" data-percent="<?php echo $temp; ?>">
 									<span class="percent"><span id="temp"><?php echo $temp; ?></span><i>°C</i></span>
-									<span class="label">Cpu Temperature Last 24H</span>
-									<div id="day" style="height:300px" class="span9"></div>
+									<span class="label">Temperature</span>
 								</span>
 							</div>
 
@@ -312,6 +311,15 @@ include_once('./include/menu.php');
 					</div>
 				</div>
 
+				<div id="active-users" class="panel panel-default" style="margin-bottom: 5px">
+					<div class="panel-heading">
+						<h3 class="panel-title">Temperatuur CPU 24H</h3>
+					</div>
+					<div class="panel-body">
+						<div id="day" style="height:300px" class="span9"></div>
+					</div>
+				</div>
+
 				<div id="top-processes" class="panel panel-default" style="margin-bottom: 5px">
 					<div class="panel-heading">
 						<h3 class="panel-title">Top Processes</h3>
@@ -359,44 +367,6 @@ include_once('./include/menu.php');
 <div id="dialog-placeholder"></div>
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
-<script type="text/javascript">
- $(document).ready(function() {
-	 var options = {
-		 xaxis: {
-			 mode: 	"time",
-			 format: "%d.%m.Y %H:%M"
-		 },
-		 yaxis: {
-			 tickFormatter: function(value) {
-				 return value.toFixed(1) + ' <?php echo strtoupper($config['unit'])?>&deg;';
-			 }
-		 },
-		 grid: {
-			 hoverable: true,
-			 clickable: true,
-		 },
-		 tooltip: true,
-		 tooltipOpts: {
-			 content: "%y - %x"
-		 }
-	 };
-	 var dataDay = [{
-		 color: "rgb(212,62, 48)",
-		 data: <?php echo json_encode($pointsDay);?>
-	 }];
-	 $.plot($("#day"), dataDay, options);
-	 var dataWeek = [{
-		 color: "rgb(212,62, 48)",
-		 data: <?php echo json_encode($pointsWeek);?>
-	 }];
-	 $.plot($("#week"), dataWeek, options);
-	 var dataOverall = [{
-		 color: "rgb(212,62, 48)",
-		 data: <?php echo json_encode($points);?>
-	 }];
-	 $.plot($("#overall"), dataOverall, options);
- });
-	 </script>
 </body>
 </body>
 </html>
